bf988b09db3245869a59228c15bcde9fa4870b89
michaeltorres1 <torresmichael068@gmail.com>
2020-04-26 01:50:36 -0400

    quick commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Fragment } from 'react';
 import Navbar from './components/common/Navbar';
 import Menu from './components/widgets/Menu';
+import MainWidget from './components/widgets/Main';
 
 const App = () => (
   <Fragment>
     <hr className='w-100 bg-primary mt-0 mb-0' />
     <Navbar />
-    <div className='d-flex'>
+    <div className='d-flex ml-3 mt-3'>
       <Menu />
+      <MainWidget />
     </div>
   </Fragment>
 );
--- src/components/widgets/Menu.js
+++ src/components/widgets/Menu.js
@@ -2,35 +2,30 @@ import React from 'react';
 
 const Menu = () => {
   return (
-    <div className='ml-4 mt-1 flex flex-column this-menu'>
-      <div
-        id='menuWidget'
-        className='flex flex-column bg-primary-light mt-4 p-2'
-      >
-        <div className='menu-link'>
-          <div className='font-weight-lighter text-light d-flex justify-content-center'>
-            MT
-          </div>
+    <div id='menuWidget' className='flex flex-column bg-primary-light mr-3 p-2'>
+      <div className='menu-link'>
+        <div className='font-weight-lighter text-light d-flex justify-content-center'>
+          MT
         </div>
-        <hr className='bg-light' />
-        <div className='mb-4 menu-link'>
-          <i className='fas fa-graduation-cap text-light fa-2x'></i>
-        </div>
-        <div className='mb-4 menu-link'>
-          <i className='fas fa-book-open text-light fa-2x'></i>
-        </div>
-        <div className='mb-4 menu-link'>
-          <i className='fab fa-js-square text-light fa-2x'></i>
-        </div>
-        <div className='mb-4 menu-link'>
-          <i class='fab fa-python text-light fa-2x'></i>
-        </div>
-        <div className='mb-4 menu-link'>
-          <i class='far fa-gem text-light fa-2x'></i>
-        </div>
-        <div className='mb-4 menu-link'>
-          <i class='fab fa-java text-light fa-2x'></i>
-        </div>
+      </div>
+      <hr className='bg-light' />
+      <div className='mb-4 menu-link'>
+        <i className='fas fa-graduation-cap text-light fa-2x'></i>
+      </div>
+      <div className='mb-4 menu-link'>
+        <i className='fas fa-book-open text-light fa-2x'></i>
+      </div>
+      <div className='mb-4 menu-link'>
+        <i className='fab fa-js-square text-light fa-2x'></i>
+      </div>
+      <div className='mb-4 menu-link'>
+        <i class='fab fa-python text-light fa-2x'></i>
+      </div>
+      <div className='mb-4 menu-link'>
+        <i class='far fa-gem text-light fa-2x'></i>
+      </div>
+      <div className='mb-4 menu-link'>
+        <i class='fab fa-java text-light fa-2x'></i>
       </div>
     </div>
   );
