aafebde615da9326e4f868f0525cd79a9c092598
michaeltorres1 <torresmichael068@gmail.com>
2020-04-26 00:38:43 -0400

    Update navbar

--- src/components/widgets/Menu.js
+++ src/components/widgets/Menu.js
@@ -2,39 +2,33 @@ import React from 'react';
 
 const Menu = () => {
   return (
-    <div className='ml-4 mt-1 flex flex-column'>
+    <div className='ml-4 mt-1 flex flex-column this-menu'>
       <div
         id='menuWidget'
         className='flex flex-column bg-primary-light mt-4 p-2'
       >
-        <div>
+        <div className='menu-link'>
           <div className='font-weight-lighter text-light d-flex justify-content-center'>
             MT
           </div>
         </div>
         <hr className='bg-light' />
-        <div className='mb-4 d-flex justify-content-center'>
+        <div className='mb-4 menu-link'>
           <i className='fas fa-graduation-cap text-light fa-2x'></i>
         </div>
-        <div className='mb-4 d-flex justify-content-center'>
+        <div className='mb-4 menu-link'>
           <i className='fas fa-book-open text-light fa-2x'></i>
         </div>
-        <div>
-          <div className='font-weight-lighter text-light d-flex justify-content-center'>
-            SKILLS
-          </div>
-        </div>
-        <hr className='bg-light' />
-        <div className='mb-4 d-flex justify-content-center'>
+        <div className='mb-4 menu-link'>
           <i className='fab fa-js-square text-light fa-2x'></i>
         </div>
-        <div className='mb-4 d-flex justify-content-center'>
+        <div className='mb-4 menu-link'>
           <i class='fab fa-python text-light fa-2x'></i>
         </div>
-        <div className='mb-4 d-flex justify-content-center'>
+        <div className='mb-4 menu-link'>
           <i class='far fa-gem text-light fa-2x'></i>
         </div>
-        <div className='mb-4 d-flex justify-content-center'>
+        <div className='mb-4 menu-link'>
           <i class='fab fa-java text-light fa-2x'></i>
         </div>
       </div>
