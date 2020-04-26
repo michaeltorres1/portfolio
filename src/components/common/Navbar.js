26c3025cc159b8c5c1dcbb10d6a4e1940deae12b
michaeltorres1 <torresmichael068@gmail.com>
2020-04-25 23:43:14 -0400

    Update navbar to include social links and removed unwanted links

--- src/components/common/Navbar.js
+++ src/components/common/Navbar.js
@@ -3,11 +3,14 @@ import React from 'react';
 const Navbar = () => {
   return (
     <nav id='nav' className='navbar bg-dark'>
-      <div className='font-weight-light text-light'>DASHBOARD</div>
+      <div id='navTitle' className='font-weight-lighter text-light'>
+        DASHBOARD
+      </div>
       <div className='search text-light'>
-        <i className='fas fa-search mr-5'></i>
-        <i className='far fa-bell mr-5'></i>
-        <i className='far fa-address-card'></i>
+        <i className='fas fa-search mr-5 nav-link'></i>
+        <i class='fab fa-github mr-5 nav-link'></i>
+        <i class='fab fa-angellist mr-5 nav-link'></i>
+        <i class='fab fa-linkedin-in nav-link'></i>
       </div>
     </nav>
   );
