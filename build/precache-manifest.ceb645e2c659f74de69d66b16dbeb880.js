f8c8448aea5f7c858504741f37784ec428d022c4
michaeltorres1 <torresmichael068@gmail.com>
2020-04-26 22:23:43 -0400

    quick commit

---
+++ build/precache-manifest.ceb645e2c659f74de69d66b16dbeb880.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0500fca157fd05a89f15d440f7a0f26",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "226ea03ef7e59903d2f8",
+    "url": "/portfolio/static/css/2.a641e29a.chunk.css"
+  },
+  {
+    "revision": "8f403128bf7b39250daf",
+    "url": "/portfolio/static/css/main.fdd62bc7.chunk.css"
+  },
+  {
+    "revision": "226ea03ef7e59903d2f8",
+    "url": "/portfolio/static/js/2.9d321c49.chunk.js"
+  },
+  {
+    "revision": "59eee5a20441efe8cf9652b2cc007836",
+    "url": "/portfolio/static/js/2.9d321c49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f403128bf7b39250daf",
+    "url": "/portfolio/static/js/main.2d5ef4d2.chunk.js"
+  },
+  {
+    "revision": "ee3d72d161f98a5d4b9d",
+    "url": "/portfolio/static/js/runtime-main.f18b6ef5.js"
+  }
+]);
