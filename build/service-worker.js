b03bc44cf184c83c21a2053b7f11aebe4fe73f7c
michaeltorres1 <torresmichael068@gmail.com>
2020-04-28 14:19:06 -0400

    quick commit

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.a566002d36b12492eba5f58424a31d1e.js"
+  "/portfolio/precache-manifest.f103bca71fe9731c7e946d79d77e0d59.js"
 );
 
 self.addEventListener('message', (event) => {
