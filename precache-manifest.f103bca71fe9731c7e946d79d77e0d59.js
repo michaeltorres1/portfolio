7810a1fdfaa1a927e501c3b0d0d63ebcc65202d5
michaeltorres1 <torresmichael068@gmail.com>
2020-04-28 14:18:53 -0400

    Updates

---
+++ precache-manifest.f103bca71fe9731c7e946d79d77e0d59.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4f49c8c007c90313aa92bb1c61ba1d6",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "0e9e7381a1e61be7e63c",
+    "url": "/portfolio/static/css/2.a641e29a.chunk.css"
+  },
+  {
+    "revision": "754073338880802d1e8f",
+    "url": "/portfolio/static/css/main.703324a9.chunk.css"
+  },
+  {
+    "revision": "0e9e7381a1e61be7e63c",
+    "url": "/portfolio/static/js/2.325e467c.chunk.js"
+  },
+  {
+    "revision": "59eee5a20441efe8cf9652b2cc007836",
+    "url": "/portfolio/static/js/2.325e467c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "754073338880802d1e8f",
+    "url": "/portfolio/static/js/main.2af317b5.chunk.js"
+  },
+  {
+    "revision": "ee3d72d161f98a5d4b9d",
+    "url": "/portfolio/static/js/runtime-main.f18b6ef5.js"
+  },
+  {
+    "revision": "db9cb916348524a05896863f8ac498fb",
+    "url": "/portfolio/static/media/michael-torres-resume.db9cb916.pdf"
+  }
+]);
