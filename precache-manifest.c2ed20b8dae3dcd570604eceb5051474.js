b39f603afbf6f5624ad32f5deca65dce05eefca7
michaeltorres1 <torresmichael068@gmail.com>
2020-04-27 20:11:38 -0400

    Updates

---
+++ precache-manifest.c2ed20b8dae3dcd570604eceb5051474.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e387e0a5e8336990e7aaa868385abd13",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "554c2bc8cfe6036bae7d",
+    "url": "/portfolio/static/css/2.a641e29a.chunk.css"
+  },
+  {
+    "revision": "c4710897c5f5dbdf6e47",
+    "url": "/portfolio/static/css/main.f25a2b23.chunk.css"
+  },
+  {
+    "revision": "554c2bc8cfe6036bae7d",
+    "url": "/portfolio/static/js/2.442de0f7.chunk.js"
+  },
+  {
+    "revision": "59eee5a20441efe8cf9652b2cc007836",
+    "url": "/portfolio/static/js/2.442de0f7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4710897c5f5dbdf6e47",
+    "url": "/portfolio/static/js/main.d2a525ab.chunk.js"
+  },
+  {
+    "revision": "ee3d72d161f98a5d4b9d",
+    "url": "/portfolio/static/js/runtime-main.f18b6ef5.js"
+  }
+]);
