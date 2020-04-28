df3c639f0d7da820e9e4233e06052bea86f71bd1
michaeltorres1 <torresmichael068@gmail.com>
2020-04-27 22:32:47 -0400

    Add pdf to right place

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,17 +1,40 @@
 import React, { Component } from 'react';
-import { Document, pdfjs } from 'react-pdf';
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+import { Document, Page } from 'react-pdf';
 
-export default class Resume extends Component {
+export default class App extends Component {
+  state = { numPages: null, pageNumber: 1 };
+
+  onDocumentLoadSuccess = ({ numPages }) => {
+    this.setState({ numPages });
+  };
+
+  goToPrevPage = () =>
+    this.setState((state) => ({ pageNumber: state.pageNumber - 1 }));
+  goToNextPage = () =>
+    this.setState((state) => ({ pageNumber: state.pageNumber + 1 }));
+
   render() {
+    const { pageNumber, numPages } = this.state;
+
     return (
       <div>
+        <nav>
+          <button onClick={this.goToPrevPage}>Prev</button>
+          <button onClick={this.goToNextPage}>Next</button>
+        </nav>
+
         <div style={{ width: 600 }}>
           <Document
-            file='michael-torres-resume.pdf'
+            file='/michael-torres-resume.pdf'
             onLoadSuccess={this.onDocumentLoadSuccess}
-          ></Document>
+          >
+            <Page pageNumber={pageNumber} width={600} />
+          </Document>
         </div>
+
+        <p>
+          Page {pageNumber} of {numPages}
+        </p>
       </div>
     );
   }
